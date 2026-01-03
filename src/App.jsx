5a5cd25c03ee60cbfd20e54fdb55f282b85c3f81
Akhil Kanawade <yashkanawade66@gmail.com>
2026-01-03 19:57:39 +0530

    Fix routing basename and vite config for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './Header';
 import Hero from './Hero';
 import Explore from './Explore';
 import ContactUs from './ContactUs';
 import Home from './Home';
+
 export default function App() {
   return (
-    <Router basename="/Brand_Page-Website-React">
+    <Router>
       <Header />
-      <Routes  >
+      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/explore" element={<Explore />} />
         <Route path="/contact" element={<ContactUs />} />
